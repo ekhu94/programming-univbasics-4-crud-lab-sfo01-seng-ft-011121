68efe4b309373b27091a1967c8771b3e3ca581d2
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 22:44:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,3 +10,14 @@ def add_element_to_end_of_array(arr, ele)
   arr << ele
   arr
 end
+
+def add_element_to_start_of_array(arr, ele)
+  arr.unshift(ele)
+  arr
+end
+
+def remove_element_from_end_of_array(arr)
+  arr.
+end
+  
+  
